c0ed9c26b739e77ca234202090ab2ebba3852185
User <dino.man21@yandex.ru>
2025-04-29 16:35:54 +0300

    Кощей сдох таки V2

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
+    ;
 
     @Autowired
-    private Island2 island;
+    private Island2 island2;
+
 
     @Override
     public String toString() {
-        return "на океане остров " + island.toString();
+        return "на океане остров " + island2.toString();
     }
 }
